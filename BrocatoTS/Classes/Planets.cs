bf516b535d1ceef6bf221e4445f85c8de7f60253
Patrick Brocato <patrickbrocato@gmail.com>
2019-04-14 23:46:20 -0400

    Initial commit
    Basic planet class has been made with some methods that might help with the project.

---
+++ BrocatoTS/Classes/Planets.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrocatoTS.Classes
+{
+    class Planets
+    {
+        int XCoordinate { get; set; }
+        int YCoordinate { get; set; }
+        string Name { get; set; }
+
+        public Planets(int x, int y, string name)
+        {
+            XCoordinate = x;
+            YCoordinate = y;
+            Name = name;
+        }
+
+        private void GeneratePlanets()
+        {
+            List<Planets> planets = new List<Planets>();
+
+            //for i <= number of planets the user inputs
+            for (int i = 0; i <= 100; i++)
+            {
+                Planets p = new Planets(0, 0, "Planets" + i.ToString());
+                if(PlanetChecker(p,planets) == false)
+                {
+                     //Make it unique
+                }
+                planets.Add(p);
+            }
+        }
+
+        private bool PlanetChecker(Planets planet, List<Planets> planetsInSystem)
+        {
+            bool isUniquePlanet = false;
+            var samePlanet = planetsInSystem.Where(p => p.XCoordinate == planet.XCoordinate && p.YCoordinate == planet.YCoordinate);
+            if(samePlanet == null)
+            {
+                isUniquePlanet = true;
+            }
+            return isUniquePlanet;
+        }
+
+        private Tuple<int, int> DistanceBetweenPlanets(Planets p1, Planets p2)
+        {
+            int xDistance = p1.XCoordinate - p2.XCoordinate;
+            int yDistance = p1.YCoordinate - p2.YCoordinate;
+
+            return Tuple.Create(xDistance, yDistance);
+        }
+    }
+}
--- BrocatoTS/Form1.cs
+++ BrocatoTS/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BrocatoTS.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,18 @@ namespace BrocatoTS
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GeneratePlanets()
+        {
+            List<Planets> planets = new List<Planets>();
+
+            //for i <= number of cities the user inputs
+            for(int i = 0; i <= 0; i++)
+            {
+                Planets p = new Planets(0, 0, "Planets" + i.ToString());
+                planets.Add(p);
+            }
         }
     }
 }
